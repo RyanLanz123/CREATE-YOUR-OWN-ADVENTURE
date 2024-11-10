06ddf5e365a6af43f1218d917d90c4aad4161d88
RyanLanz123 <ryanlanz6@gmail.com>
2024-11-10 14:01:20 +0000

    add different paths and options using if/elif statements

---
+++ chose_your_own_adventure.py
@@ -0,0 +1,13 @@
+name = input("Type your name:")
+print("Welcome", name, "to this adventure")
+
+answer = input("You are on a dirt road, it has come to and you can go left or right, which way would you like to go?").lower()
+
+if answer == "left":
+    answer = input("You come to a river, you can walk around it or swin accross? Type walk to walk around and swim to swim across: ")
+
+elif answer == "right":
+    print()
+
+else:
+    print("Not a valid option, you lose")
